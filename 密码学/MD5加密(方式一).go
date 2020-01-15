d2f4530bbb9fb046550d2fd98d94556969b75066
itbsl <itbsl@foxmail.com>
2020-01-15 22:23:36 +0800

    修改MD5加密函数说明注释

--- 密码学/MD5加密(方式一).go
+++ 密码学/MD5加密(方式一).go
@@ -6,11 +6,11 @@ import (
 	"fmt"
 )
 
-//计算MD5的方式
+//计算数据的MD5值
 func getMD5(data []byte) string {
 	//1.计算数据data的MD5校验和
 	result := md5.Sum(data)
-	//2.将数据src编码为16进制字符串s，每个字节占两位，不足补0。
+	//2.将数据src编码为16进制字符串s，每个字节占两位，不足补0，最终返回一个32位长度的字符串
 	return hex.EncodeToString(result[:])
 }
 
--- 密码学/MD5加密(方式二).go
+++ 密码学/MD5加密(方式二).go
@@ -7,7 +7,7 @@ import (
 	"io"
 )
 
-//计算数据的MD5
+//计算数据的MD5值
 func getMD5(str string) string {
 	//1.创建一个使用MD5校验的hash.Hash接口
 	hash := md5.New()
@@ -18,7 +18,7 @@ func getMD5(str string) string {
 	//3.计算结果
 	result := hash.Sum(nil)
 
-	//4.将数据src编码为16进制字符串s，每个字节占两位，不足补0。
+	//4.将数据src编码为16进制字符串s，每个字节占两位，不足补0，最终返回一个32位长度的字符串
 	//return fmt.Sprintf("%x", result)
 	return hex.EncodeToString(result[:])
 }
